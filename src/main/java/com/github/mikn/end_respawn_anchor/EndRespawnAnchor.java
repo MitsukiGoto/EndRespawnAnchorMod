08844e2a1fd9c88db32e3e443fa6efa1da2bf826
MitsukiGoto <mi22kknsinge@ezweb.ne.jp>
2022-02-27 20:11:22 +0900

    initalized commit

---
+++ src/main/java/com/github/mikn/end_respawn_anchor/EndRespawnAnchor.java
@@ -0,0 +1,13 @@
+package com.github.mikn.end_respawn_anchor;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(EndRespawnAnchor.MODID)
+public class EndRespawnAnchor {
+    public static final String MODID = "end_respawn_anchor";
+    public static final Logger LOGGER = LogManager.getLogger("EndRespawnAnchor/Main");
+    public EndRespawnAnchor() {
+    }
+}
