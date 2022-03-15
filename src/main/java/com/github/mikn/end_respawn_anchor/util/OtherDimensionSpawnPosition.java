b23d3dffa30999a5658d205364cd209cf94bf0b1
MitsukiGoto <mi22kknsinge@ezweb.ne.jp>
2022-03-15 15:42:03 +0900

    implement respawn_position_check command

--- src/main/java/com/github/mikn/end_respawn_anchor/util/OtherDimensionSpawnPosition.java
+++ src/main/java/com/github/mikn/end_respawn_anchor/util/OtherDimensionSpawnPosition.java
@@ -5,4 +5,19 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.Level;
 
 public record OtherDimensionSpawnPosition(ResourceKey<Level> dimension, BlockPos blockPos, float respawnAngle) {
+    @Override
+    public String toString() {
+        return "Dimension: " + provideDimension(dimension) + " BlockPos: " + blockPos.toString();
+    }
+
+    private String provideDimension(ResourceKey<Level> level) {
+        if (level == Level.OVERWORLD) {
+            return "overworld";
+        } else if (level == Level.NETHER) {
+            return "nether";
+        } else if (level == Level.END) {
+            return "end";
+        }
+        return "Other dimension";
+    }
 }
