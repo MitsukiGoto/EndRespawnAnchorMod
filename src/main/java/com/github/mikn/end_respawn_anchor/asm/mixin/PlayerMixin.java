c0468094dceea85577521ed4352ddcc423f4801f
Mitsuki Goto <48896872+MitsukiGoto@users.noreply.github.com>
2022-10-29 22:57:58 +0900

    making EndRespawnAnchor work

--- src/main/java/com/github/mikn/end_respawn_anchor/asm/mixin/PlayerMixin.java
+++ src/main/java/com/github/mikn/end_respawn_anchor/asm/mixin/PlayerMixin.java
@@ -21,14 +21,16 @@
 
 package com.github.mikn.end_respawn_anchor.asm.mixin;
 
-import com.github.mikn.end_respawn_anchor.EndRespawnAnchor;
-import com.github.mikn.end_respawn_anchor.event.FindRespawnPositionAndUseSpawnBlockEvent;
+import com.github.mikn.end_respawn_anchor.block.EndRespawnAnchorBlock;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
-import net.minecraftforge.common.MinecraftForge;
 import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -37,13 +39,17 @@ import java.util.Optional;
 
 @Mixin(Player.class)
 public class PlayerMixin {
-    @Inject(method = "findRespawnPositionAndUseSpawnBlock(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;FZZ)Ljava/util/Optional;", at = @At("HEAD"), cancellable = true)
-    private static void inject(ServerLevel level, BlockPos blockPos, float respawnAngle, boolean isRespawnForced, boolean flag, CallbackInfoReturnable<Optional<Vec3>> cir) {
-        FindRespawnPositionAndUseSpawnBlockEvent evt = new FindRespawnPositionAndUseSpawnBlockEvent(level, blockPos, flag);
-        MinecraftForge.EVENT_BUS.post(evt);
-        EndRespawnAnchor.LOGGER.error(evt.getResult());
-        if (evt.getResult() == net.minecraftforge.eventbus.api.Event.Result.ALLOW) {
-            cir.setReturnValue(evt.getRespawnPosition());
+    @Inject(method= "findRespawnPositionAndUseSpawnBlock(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;FZZ)Ljava/util/Optional;", at = @At("HEAD"), cancellable = true)
+    private static void inject(ServerLevel p_36131_, BlockPos p_36132_, float p_36133_, boolean p_36134_, boolean p_36135_, CallbackInfoReturnable<Optional<Vec3>> cir) {
+        BlockState blockstate = p_36131_.getBlockState(p_36132_);
+        Block block = blockstate.getBlock();
+        if (block instanceof EndRespawnAnchorBlock && blockstate.getValue(EndRespawnAnchorBlock.CHARGE) > 0 && EndRespawnAnchorBlock.isEnd(p_36131_) && !p_36131_.isClientSide) {
+            Optional<Vec3> optional = EndRespawnAnchorBlock.findStandUpPosition(EntityType.PLAYER, p_36131_, p_36132_);
+            if (!p_36135_ && optional.isPresent()) {
+                p_36131_.setBlock(p_36132_, blockstate.setValue(EndRespawnAnchorBlock.CHARGE, Integer.valueOf(blockstate.getValue(EndRespawnAnchorBlock.CHARGE) - 1)), 3);
+            }
+            cir.setReturnValue(optional);
         }
+
     }
 }
