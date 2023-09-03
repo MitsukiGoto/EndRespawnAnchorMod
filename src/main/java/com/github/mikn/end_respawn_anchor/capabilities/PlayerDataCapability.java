4936979135d7d5c156227c1f2c983e3824183fa0
Mitsuki Goto <48896872+MitsukiGoto@users.noreply.github.com>
2023-09-04 02:37:18 +0900

    Add null check

--- src/main/java/com/github/mikn/end_respawn_anchor/capabilities/PlayerDataCapability.java
+++ src/main/java/com/github/mikn/end_respawn_anchor/capabilities/PlayerDataCapability.java
@@ -13,9 +13,11 @@ import net.minecraft.world.level.Level;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityManager;
 import net.minecraftforge.common.capabilities.CapabilityToken;
+import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
 
-public class PlayerDataCapability implements IPlayerDataCapability{
-    public static final Capability<IPlayerDataCapability> INSTANCE = CapabilityManager.get(new CapabilityToken<>() {});
+public class PlayerDataCapability implements IPlayerDataCapability {
+    public static final Capability<IPlayerDataCapability> INSTANCE = CapabilityManager.get(new CapabilityToken<>() {
+    });
     public static final String NBT_KEY_PLAYER_SPAWN_DIMENSION = "preSpawnDimension";
     public static final String NBT_KEY_PLAYER_SPAWN_POS_X = "preSpawnPosX";
     public static final String NBT_KEY_PLAYER_SPAWN_POS_Y = "preSpawnPosY";
@@ -37,24 +39,36 @@ public class PlayerDataCapability implements IPlayerDataCapability{
     @Override
     public CompoundTag serializeNBT() {
         final CompoundTag tag = new CompoundTag();
-        tag.putString(NBT_KEY_PLAYER_SPAWN_DIMENSION, this.respawnData.dimension().toString());
-        tag.putInt(NBT_KEY_PLAYER_SPAWN_POS_X, this.respawnData.blockPos().getX());
-        tag.putInt(NBT_KEY_PLAYER_SPAWN_POS_Y, this.respawnData.blockPos().getY());
-        tag.putInt(NBT_KEY_PLAYER_SPAWN_POS_Z, this.respawnData.blockPos().getZ());
-        tag.putFloat(NBT_KEY_PLAYER_SPAWN_ANGLE, this.respawnData.respawnAngle());
+        if(this.respawnData != null) {
+            tag.putString(NBT_KEY_PLAYER_SPAWN_DIMENSION, this.respawnData.dimension().location().toString());
+            tag.putInt(NBT_KEY_PLAYER_SPAWN_POS_X, this.respawnData.blockPos().getX());
+            tag.putInt(NBT_KEY_PLAYER_SPAWN_POS_Y, this.respawnData.blockPos().getY());
+            tag.putInt(NBT_KEY_PLAYER_SPAWN_POS_Z, this.respawnData.blockPos().getZ());
+            tag.putFloat(NBT_KEY_PLAYER_SPAWN_ANGLE, this.respawnData.respawnAngle());
+        } else {
+            tag.putString(NBT_KEY_PLAYER_SPAWN_DIMENSION, Level.OVERWORLD.location().toString());
+            tag.putInt(NBT_KEY_PLAYER_SPAWN_POS_X, 0);
+            tag.putInt(NBT_KEY_PLAYER_SPAWN_POS_Y, 0);
+            tag.putInt(NBT_KEY_PLAYER_SPAWN_POS_Z, 0);
+            tag.putFloat(NBT_KEY_PLAYER_SPAWN_ANGLE, 0.0f);
+        }
         return tag;
     }
 
     @Override
     public void deserializeNBT(CompoundTag nbt) {
-        if(nbt.contains(NBT_KEY_PLAYER_SPAWN_POS_X)) {
-            ResourceKey<Level> dimension = Level.RESOURCE_KEY_CODEC.parse(NbtOps.INSTANCE, nbt.get(NBT_KEY_PLAYER_SPAWN_DIMENSION)).resultOrPartial(EndRespawnAnchor.LOGGER::error).orElse(Level.OVERWORLD);
-            int posX = nbt.getInt(NBT_KEY_PLAYER_SPAWN_POS_X);
-            int posY = nbt.getInt(NBT_KEY_PLAYER_SPAWN_POS_Y);
-            int posZ = nbt.getInt(NBT_KEY_PLAYER_SPAWN_POS_Z);
-            float angle = nbt.getFloat(NBT_KEY_PLAYER_SPAWN_ANGLE);
-            this.respawnData = new RespawnData(dimension, new BlockPos(posX, posY, posZ), angle);
-        }
+        ResourceKey<Level> dimension = Level.RESOURCE_KEY_CODEC
+                .parse(NbtOps.INSTANCE, nbt.get(NBT_KEY_PLAYER_SPAWN_DIMENSION))
+                .resultOrPartial(EndRespawnAnchor.LOGGER::error).orElse(Level.OVERWORLD);
+        int posX = nbt.getInt(NBT_KEY_PLAYER_SPAWN_POS_X);
+        int posY = nbt.getInt(NBT_KEY_PLAYER_SPAWN_POS_Y);
+        int posZ = nbt.getInt(NBT_KEY_PLAYER_SPAWN_POS_Z);
+        float angle = nbt.getFloat(NBT_KEY_PLAYER_SPAWN_ANGLE);
+        BlockPos blockPos = (posX == 0 && posY == 0 && posZ == 0) ? null : new BlockPos(posX, posY, posZ);
+        this.respawnData = new RespawnData(dimension, blockPos, angle);
     }
-    
+
+    public static void register(RegisterCapabilitiesEvent event) {
+        event.register(IPlayerDataCapability.class);
+    }
 }
