21d4a378672cd1ab17244041ffb081ffdfe7d8f5
Mitsuki Goto <48896872+MitsukiGoto@users.noreply.github.com>
2023-09-13 13:26:43 +0900

    optimise unused imports

--- src/main/java/com/github/mikn/end_respawn_anchor/asm/mixin/ServerPlayerMixin.java
+++ src/main/java/com/github/mikn/end_respawn_anchor/asm/mixin/ServerPlayerMixin.java
@@ -7,14 +7,12 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtOps;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ServerPlayer.class)
 public class ServerPlayerMixin implements IServerPlayerMixin {
